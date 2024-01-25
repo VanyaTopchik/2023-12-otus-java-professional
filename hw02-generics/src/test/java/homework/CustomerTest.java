61c71798e535e463ab9c61601bfd5497766b1913
VanyaTopchik <gvanya97@mail.ru>
2024-01-25 20:19:19 +0300

    Added realization on classes

--- hw02-generics/src/test/java/homework/CustomerTest.java
+++ hw02-generics/src/test/java/homework/CustomerTest.java
@@ -3,13 +3,10 @@ package homework;
 import java.util.HashMap;
 import java.util.Map;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class CustomerTest {
-
-  // Все тесты должны проходить, менять тесты не надо.
 
   @Test
   @DisplayName("Проверяем, что класс Customer не сломан")
@@ -27,7 +24,6 @@ class CustomerTest {
   }
 
   @Test
-  @Disabled("надо удалить") // надо удалить
   @DisplayName("Объект Customer как ключ в карте")
   void customerAsKeyTest() {
     // given
@@ -55,7 +51,6 @@ class CustomerTest {
   }
 
   @Test
-  @Disabled("надо удалить") // надо удалить
   @DisplayName("Сортировка по полю score, итерация по возрастанию")
   void scoreSortingTest() {
     // given
@@ -74,9 +69,6 @@ class CustomerTest {
     assertThat(smallestScore.getKey()).isEqualTo(customer2);
 
     // when
-    // подсказка:
-    // a key-value mapping associated with the least key strictly greater than the given key, or
-    // null if there is no such key.
     Map.Entry<Customer, String> middleScore = customerService.getNext(new Customer(10, "Key", 20));
     // then
     assertThat(middleScore.getKey()).isEqualTo(customer1);
@@ -95,7 +87,6 @@ class CustomerTest {
   }
 
   @Test
-  @Disabled("надо удалить") // надо удалить
   @DisplayName("Модификация коллекции")
   void mutationTest() {
     // given
@@ -117,7 +108,6 @@ class CustomerTest {
   }
 
   @Test
-  @Disabled("надо удалить") // надо удалить
   @DisplayName("Возвращание в обратном порядке")
   void reverseOrderTest() {
     // given
