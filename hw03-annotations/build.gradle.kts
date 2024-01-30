fd126353413c483dc4b8528e293fb832b9e26763
VanyaTopchik <gvanya97@mail.ru>
2024-01-30 19:08:31 +0300

    Added processor for processing annotations

--- hw03-annotations/build.gradle.kts
+++ hw03-annotations/build.gradle.kts
@@ -9,10 +9,5 @@ repositories {
 }
 
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
-}
-
-tasks.getByName<Test>("test") {
-    useJUnitPlatform()
+    implementation ("ch.qos.logback:logback-classic")
 }
