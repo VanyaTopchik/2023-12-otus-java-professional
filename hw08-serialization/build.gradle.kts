5356fa873935b4d25458edfbf90c050774d3a414
VanyaTopchik <gvanya97@mail.ru>
2024-03-17 22:14:37 +0300

    Added code for json serialization

--- hw08-serialization/build.gradle.kts
+++ hw08-serialization/build.gradle.kts
@@ -10,6 +10,7 @@ repositories {
 
 dependencies {
     implementation ("ch.qos.logback:logback-classic")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
     testImplementation ("org.junit.jupiter:junit-jupiter-api")
     testImplementation ("org.junit.jupiter:junit-jupiter-engine")
     testImplementation ("org.assertj:assertj-core")
